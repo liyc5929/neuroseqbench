b00a0589730a4da167344225472534ea8048c7ab
liyc5929 <48726053+liyc5929@users.noreply.github.com>
2025-04-16 23:25:51 +0800

    Add raw version of full AL experiments for neuromorphic sequential arena.

--- src/neuroseqbench/network/structure/__init__.py
+++ src/neuroseqbench/network/structure/__init__.py
@@ -5,3 +5,4 @@ from .lstm import LSTMNet, LMLSTM
 from .temporal_convolutional_network import TCN, LMTCN
 from .transformer import TransformerNet, LMTransformer
 from .spike_driven_transformer import SpkTransformerNet, LMSpkTransformer
+from .state_space_model import SSM
