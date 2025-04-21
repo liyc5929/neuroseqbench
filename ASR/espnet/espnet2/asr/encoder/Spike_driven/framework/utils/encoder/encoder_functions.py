27ca4188e55cebece01a9d82a5b0602a4178553c
liyc5929 <48726053+liyc5929@users.noreply.github.com>
2025-04-21 12:52:31 +0800

    Relocated AD and ASR experiments for neuromorphic sequential arena.

--- ASR/espnet/espnet2/asr/encoder/Spike_driven/framework/utils/encoder/encoder_functions.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-
-def rate_coding(x, time_step, lower_bound: int = 0, upper_bound: int = 1):
-    coded_list = [
-        (x > ((upper_bound - lower_bound) * torch.rand(x.shape) + lower_bound)).float()
-        for _ in range(time_step)
-    ]
-    return torch.stack(coded_list)
