a8ede02dae5e4d6196efe2e0c13080b424fafc6c
xy_chen <103318375+xychen-comp@users.noreply.github.com>
2025-05-26 22:53:32 +0800

    Update parallel_multi_compartment_spiking_neuron.py
    Update PMSN model

--- src/neuroseqbench/network/neuron/parallel_multi_compartment_spiking_neuron.py
+++ src/neuroseqbench/network/neuron/parallel_multi_compartment_spiking_neuron.py
@@ -28,7 +28,7 @@ class PMSN(BaseNeuron):
         self.recurrent = recurrent
         self.surro_grad= surro_grad
         if self.recurrent:
-            self.recurrent_weight = nn.Linear(self.neuron_num, self.neuron_num)
+            raise ValueError("PMSN does not support recurrent mode.")
         self.return_mem = False
 
         self.kernel = PMSN_kernel(self.neuron_num, N=4)
@@ -55,17 +55,15 @@ class PMSN(BaseNeuron):
             return_state = True
         else:
             return_state = False
-        step_num = tx.size(0) #(T,B,H)
+        step_num = tx.size(0) 
 
         tx = self.bn(tx.view(-1, tx.size(-1))).view(step_num, -1, self.neuron_num)
-        tx = tx.permute(1,2,0) #(B, H, T)
-        # Compute SSM Kernel
-        k = self.kernel(L=step_num) # (H T)
+        tx = tx.permute(1,2,0) # [B H T]
+        k = self.kernel(L=step_num) # [H T]
 
-        # # Convolution
-        k_f = torch.fft.rfft(k, n=2*step_num) # (H T)
-        u_f = torch.fft.rfft(tx, n=2*step_num) # (B H T)
-        _y = torch.fft.irfft(u_f*k_f, n=2*step_num)[..., :step_num] # (B H T)
+        k_f = torch.fft.rfft(k, n=2*step_num) # [H T]
+        u_f = torch.fft.rfft(tx, n=2*step_num) # [B H T]
+        _y = torch.fft.irfft(u_f*k_f, n=2*step_num)[..., :step_num] # [B H T]
         y = _y + (tx * self.D.unsqueeze(-1))
         # proposed reset mechanism
         ty = PMSN_surrogate.apply(y.relu(), self.thresh.to(tx.device))
@@ -81,7 +79,6 @@ class PMSN(BaseNeuron):
 class PMSN_kernel(nn.Module):
     def __init__(self, d_model, N=4, dt_min=1e-3, dt_max=1e-1):
         super().__init__()
-        # Generate dt
         H = d_model
         log_dt = torch.rand(H).uniform_(0, 1) * (math.log(dt_max) - math.log(dt_min)) + math.log(dt_min)  # [H]
 
@@ -91,9 +88,9 @@ class PMSN_kernel(nn.Module):
         super_diag_indices = diag_indices[1:] - 1
 
         S = torch.zeros(N,N)
-        S[diag_indices, diag_indices] = -0.5
-        S[diag_indices[:-1], sub_diag_indices] = 5. * ((torch.arange(N-1)+1))
-        S[diag_indices[1:], super_diag_indices] = -5. * ((torch.arange(N-1)+1)) # 超对角线
+        S[diag_indices, diag_indices] = -0.5 # main diagonal
+        S[diag_indices[:-1], sub_diag_indices] = 5. * ((torch.arange(N-1)+1)) # superdiagonal
+        S[diag_indices[1:], super_diag_indices] = -5. * ((torch.arange(N-1)+1)) # subdiagonal
 
         S_diag = torch.diagonal(S)
         A_real = (torch.mean(S_diag) * torch.ones_like(S_diag)).unsqueeze(0).repeat(H,1)
@@ -105,14 +102,13 @@ class PMSN_kernel(nn.Module):
         self.mask[diag_indices, diag_indices] = 1
         self.mask[diag_indices[:-1], sub_diag_indices] = 1
 
-
         log_A_real = torch.log(-A_real)
         self.log_A_real = nn.Parameter(log_A_real)
         self.A_imag = nn.Parameter(A_imag)
 
         B = torch.ones(H, N)
         C= torch.zeros(H,N)
-        C[:,-1]=1
+        C[:,-1] = 1
         Vinv=V.conj().T  # [N,N]
         CV= torch.einsum('hm,mn->hn',C+0j,V) # [H,N]
         VinvB=torch.einsum('mn,hn->hm',Vinv,B+0j) #[H,N]
@@ -123,18 +119,17 @@ class PMSN_kernel(nn.Module):
         self.CV_imag = nn.Parameter(CV.imag)
 
     def forward(self, L):
-        # u [B,H,L]
-        A = -torch.exp(self.log_A_real) + 1j * self.A_imag  # (H N)
+        A = -torch.exp(self.log_A_real) + 1j * self.A_imag  #[H,N]
         B = self.VinvB_real + 1j * self.VinvB_imag  #(H,N)
         C = self.CV_real + self.CV_imag * 1j
 
         # Materialize parameters
         dt = torch.exp(self.log_dt)  # (H,1)
-        A_bar = torch.exp(A*dt.unsqueeze(-1))  #[H N]
+        A_bar = torch.exp(A*dt.unsqueeze(-1))  #[H,N]
         B_bar = (A_bar-1)*B/A
         # Vandermonde multiplication
-        logK = (A*dt.unsqueeze(-1)).unsqueeze(-1) * torch.arange(L, device=A.device) # (H N L)   e-At
+        logK = (A*dt.unsqueeze(-1)).unsqueeze(-1) * torch.arange(L, device=A.device) # [H,N,L]
         K = torch.exp(logK)
-        KB = torch.einsum('hnl,hn->hnl',K,B_bar)  # e-At*B  # (H N L)
-        CKB = torch.einsum('hn, hnl -> hl', C, KB).real #(H L)
+        KB = torch.einsum('hnl,hn->hnl',K,B_bar) 
+        CKB = torch.einsum('hn, hnl -> hl', C, KB).real #[H,L]
         return CKB
